ec6ab2333baf255210b7f1175fd9076593ebdcdb
Aubrey Agub <2555752a@student.gla.ac.uk>
2024-10-12 22:00:32 +0100

    Added missing path for fraser building to reading room, factored out paths to another file

--- agent.py
+++ agent.py
@@ -1,70 +1,8 @@
 from building import Building
 import random
+import paths
 
 PERSON_SPEED = 2.0
-
-bo_to_lib = [(55.873498, -4.292804), 
-                (55.873311, -4.293180), 
-                (55.873010, -4.292456),
-                (55.872712, -4.291050), 
-                (55.872530, -4.289391),
-                (55.872807, -4.288762),
-                (55.873128, -4.288531),
-                (55.873323, -4.288474)]
-
-lib_to_rr = [(55.873323, -4.288474), 
-                (55.873102, -4.288537), 
-                (55.873065, -4.288317), 
-                (55.872884, -4.288315), 
-                (55.872674, -4.288206), 
-                (55.872346, -4.288193)]
-
-rr_to_bo = [(55.872346, -4.288193),
-            (55.872452, -4.289079),
-            (55.872521, -4.289712),
-            (55.872681, -4.290329),
-            (55.872889, -4.290934),
-            (55.873012, -4.291090),
-            (55.873137, -4.291259),
-            (55.873302, -4.291524),
-            (55.873498, -4.292804)] 
-
-bo_to_fraser = [(55.873498, -4.292804), 
-                (55.873311, -4.293180), 
-                (55.873010, -4.292456),
-                (55.872712, -4.291050), 
-                (55.872530, -4.289391),
-                (55.872807, -4.288762),
-                (55.873128, -4.288531),
-                (55.873218, -4.288445)]
-
-lib_to_fraser = [(55.873323, -4.288474),(55.873218, -4.288445)]  
-
-fraser_to_rr =  [(55.873218, -4.288445),
-                (55.873102, -4.288537), 
-                (55.873065, -4.288317), 
-                (55.872884, -4.288315), 
-                (55.872674, -4.288206), 
-                (55.872346, -4.288193)]
-
-
-        
-paths = {
-    ("boyd orr", "library"): bo_to_lib,
-    ("library", "boyd orr"): bo_to_lib[::-1],
-    ("library", "reading room"): lib_to_rr,
-    ("reading room", "library"): lib_to_rr[::-1],
-    ("reading room", "boyd orr"): rr_to_bo,
-    ("boyd orr", "reading room"): rr_to_bo[::-1],
-    ("library", "fraser building"): lib_to_fraser,
-    ("fraser building", "library"): lib_to_fraser[::-1],
-    ("boyd orr", "fraser building"): bo_to_fraser,
-    ("fraser building", "boyd orr"): bo_to_fraser[::-1],
-    ("boyd orr", "boyd orr"): [(55.873498, -4.292804)],
-    ("library", "library"): [(55.873323, -4.288474)],
-    ("reading room", "reading room"): [(55.872346, -4.288193)],
-    ("fraser building", "fraser building"): [(55.873218, -4.288445)]
-}
 
 class Agent():
     def __init__(self, name: str, course: str, stink_threshold: float, building_preferences: tuple, stink=0.0):
@@ -132,8 +70,8 @@ class Agent():
 
 def get_path(start, end):
         path_key = (start,end)
-        if path_key in paths:  
-            return paths[path_key].copy()
+        if path_key in paths.paths:  
+            return paths.paths[path_key].copy()
         else:
             print(f"Path not found between {start} and {end}")
             return []
